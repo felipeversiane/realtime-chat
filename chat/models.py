cda60c682447e3010c0f08e5686000722823fd62
felipeversiane <felipeversiane09@gmail.com>
2024-11-15 22:17:17 -0300

    feat: initial sync chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Modifier(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid1, unique=True, primary_key=True, editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        
+class ChatGroup(Modifier):
+    group_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+    
+    def __str__(self):
+        return "{}".format(self.group_name)
+
+class GroupMessage(Modifier):
+    group = models.ForeignKey(ChatGroup,related_name='chat_messages',on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return "{} : {}".format(self.author.username, self.body)
+    
+    class Meta:
+        ordering = ['created_at']
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
 
-# Create your views here.
+@login_required
 def chat_view(request):
-    return render(request, 'chat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name="Study")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(request, 'chat/chat.html', {'chat_messages': chat_messages})
